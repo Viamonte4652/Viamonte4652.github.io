d1a028370faaff9136808f3a90903c3f8d02dcea
Viamonte4652 <86967093+Viamonte4652@users.noreply.github.com>
2023-02-15 17:18:42 +1000

    Add files via upload

---
+++ assets/js/text.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(".typed").typed({
+      strings: [
+        "Привет!",
+        "Hi!",
+        "Как дела?",
+        "How's it going?"
+      ],
+      typeSpeed: 10,
+      backSpeed: 0,
+      cursorChar: ["❚"],
+      smartBackspace: true,
+      fadeOut: true,
+      loop: true
+    });
+  });  
